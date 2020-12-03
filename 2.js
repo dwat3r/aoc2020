50c62e69e631b9c6610f8493cdecadcc07ca7809
dwat3r <hechtloliver@gmail.com>
2020-12-03 11:08:23 +0100

    format

--- 2.js
+++ 2.js
@@ -1,5 +1,20 @@
-const input = document.querySelector("body > pre").textContent.split('\n')
+const input = document.querySelector("body > pre").textContent.split("\n");
 // first part
-input.map(i => i.match(/(\d+)-(\d+)\s+([a-z]):\s+([a-z]+)/)).filter(x => x !== null).filter(i => {let [x,from,to,c,passw] = i;let l = passw.split("").filter(p => p === c).length; return l >= parseInt(from) && l <= parseInt(to)}).length
+input
+  .map((i) => i.match(/(\d+)-(\d+)\s+([a-z]):\s+([a-z]+)/))
+  .filter((x) => x !== null)
+  .filter((i) => {
+    let [x, from, to, c, passw] = i;
+    let l = passw.split("").filter((p) => p === c).length;
+    return l >= parseInt(from) && l <= parseInt(to);
+  }).length;
 // second part
-input.map(i => i.match(/(\d+)-(\d+)\s+([a-z]):\s+([a-z]+)/)).filter(x => x !== null).filter(i => {let [x,from,to,c,passw] = i;let a = passw.split("")[parseInt(from)-1] === c; let b = passw.split("")[parseInt(to)-1] === c;return a ? !b : b}).length
+input
+  .map((i) => i.match(/(\d+)-(\d+)\s+([a-z]):\s+([a-z]+)/))
+  .filter((x) => x !== null)
+  .filter((i) => {
+    let [x, from, to, c, passw] = i;
+    let a = passw.split("")[parseInt(from) - 1] === c;
+    let b = passw.split("")[parseInt(to) - 1] === c;
+    return a ? !b : b;
+  }).length;
