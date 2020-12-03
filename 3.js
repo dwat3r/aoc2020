59fdce337dda8a4d20d9f257210ae0eafe3e82fb
Oliver Hechtl <hechtloliver@gmail.com>
2020-12-03 10:13:06 +0100

    Update 3.js

--- 3.js
+++ 3.js
@@ -1,4 +1,4 @@
 const input = document.querySelector("body > pre").textContent.split('\n').map(i => i.split(""))
 // first part
-input.reduce((acc, i) => ({x: ((acc.x + 3) % 31), res: (acc.res + (i[acc.x] === "#" ? 1 : 0))}), {x: 0,res: 0})
+input.reduce((acc, i) => ({x: ((acc.x + 3) % 31), res: (acc.res + (i[acc.x] === "#" ? 1 : 0))}), {x: 0,res: 0}).res
 // second part
