27a47a472faa46d007cf405bc7d338524dc344d1
dwat3r <hechtloliver@gmail.com>
2020-12-07 11:11:53 +0100

    format

--- 6.js
+++ 6.js
@@ -1,5 +1,22 @@
-const input = document.querySelector("pre").textContent.split("\n\n")
+const input = document.querySelector("pre").textContent.split("\n\n");
 // first part
-input.map(i => i.split("").filter(i => i.match(/[a-z]/)).reduce((set,x)=>set.add(x),new Set()).size).reduce((acc,x)=>acc+x)
+input
+  .map(
+    (i) =>
+      i
+        .split("")
+        .filter((i) => i.match(/[a-z]/))
+        .reduce((set, x) => set.add(x), new Set()).size
+  )
+  .reduce((acc, x) => acc + x);
 // second part
-input.map(i => i.split("\n").filter(i => i.length !== 0).map(i => [...i]).reduce((acc,x)=>acc.filter(i => x.includes(i))).length).reduce((acc,x)=>acc+x)
+input
+  .map(
+    (i) =>
+      i
+        .split("\n")
+        .filter((i) => i.length !== 0)
+        .map((i) => [...i])
+        .reduce((acc, x) => acc.filter((i) => x.includes(i))).length
+  )
+  .reduce((acc, x) => acc + x);
--- 7.js
+++ 7.js
@@ -1,8 +1,51 @@
-let input = document.querySelector("body > pre").textContent.split('.\n')
+let input = document.querySelector("body > pre").textContent.split(".\n");
 // first part
-let map = input.filter(i => i !== "").reduce((map,i) => ({...map, [i.match(/^\w+ \w+/)[0]]: (i.indexOf("no") === -1 ? i.slice(i.indexOf("contain") + 8).split(", ").map(i => i.match(/^(\d+) (\w+ \w+)/).slice(1,3)) : [])}), {})
-let findShiny = (ss,mmap) => { let map = {...mmap};let c = Object.entries(map).filter(kv => kv[1].some(cb => ss.includes(cb[1])));c.forEach(kv => delete map[kv[0]]);if (c.length === 0) {return 0} else {return c.length + findShiny(c.map(kv => kv[0]),{...map})}}
-findShiny(["shiny gold"],map)
+let map = input
+  .filter((i) => i !== "")
+  .reduce(
+    (map, i) => ({
+      ...map,
+      [i.match(/^\w+ \w+/)[0]]:
+        i.indexOf("no") === -1
+          ? i
+              .slice(i.indexOf("contain") + 8)
+              .split(", ")
+              .map((i) => i.match(/^(\d+) (\w+ \w+)/).slice(1, 3))
+          : [],
+    }),
+    {}
+  );
+let findShiny = (ss, mmap) => {
+  let map = { ...mmap };
+  let c = Object.entries(map).filter((kv) =>
+    kv[1].some((cb) => ss.includes(cb[1]))
+  );
+  c.forEach((kv) => delete map[kv[0]]);
+  if (c.length === 0) {
+    return 0;
+  } else {
+    return (
+      c.length +
+      findShiny(
+        c.map((kv) => kv[0]),
+        { ...map }
+      )
+    );
+  }
+};
+findShiny(["shiny gold"], map);
 // second part
-let countShiny = (ss,mmap) => {let map = {...mmap};if (map[ss].length === 0) {return 0} else {return map[ss].map(cb => parseInt(cb[0]) + parseInt(cb[0]) * countShiny(cb[1], {...map})).reduce((acc,x)=>acc+x)}}
-countShiny("shiny gold",map)
+let countShiny = (ss, mmap) => {
+  let map = { ...mmap };
+  if (map[ss].length === 0) {
+    return 0;
+  } else {
+    return map[ss]
+      .map(
+        (cb) =>
+          parseInt(cb[0]) + parseInt(cb[0]) * countShiny(cb[1], { ...map })
+      )
+      .reduce((acc, x) => acc + x);
+  }
+};
+countShiny("shiny gold", map);
