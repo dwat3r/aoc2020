e40bae9bb8e1899ca2435592c484db5a6b40a2c9
dwat3r <hechtloliver@gmail.com>
2020-12-22 12:55:27 +0100

    day22 too slow solution

--- 22.js
+++ 22.js
@@ -1,4 +1,5 @@
 var _ = require('lodash');
+//var hash = require('object-hash');
 
 let test = `Player 1:
 9
@@ -114,24 +115,54 @@ let score = (gs) => {
 // console.log(score(play(parse(input))))
 
 // part2
-let deepIncludes = (arr,e) => arr.filter(ae => _.isEqual(ae,e)).length !== 0
+// let hash = gs => 
+//   7919*gs.p1.map((x,ix) => x*ix).reduce((acc,x)=>acc+x,1)+7727*
+//   gs.p2.map((x,ix) => x*ix).reduce((acc,x)=>acc+x,1)
+
+let hash = gs => `${gs.p1.join(",")} ${gs.p2.join(",")}`
+
 
 let playRec = (gs) => {
+  //let memo = new Map()
   return (function round(gs, prevRounds) {
-    if (gs.p2.length === 0 || deepIncludes(prevRounds,gs)) {
-      //if (deepIncludes(prevRounds,gs)) console.log(prevRounds,gs)
-      return { gs: gs, winner: 1 };
+    //if(memo.size > 0 && memo.size % 1000000 === 0) console.log(memo.size)
+    // if(memo.has(hash(gs))) {
+    //   // if(!_.isEqual(memo.get(hash(gs)).gs, gs)) {
+    //   //   console.log(memo.get(hash(gs)).gs, gs)
+    //   //   throw Error(`Hash collision: `)
+    //   // }
+    //   return memo.get(hash(gs))
+    // }
+    if (gs.p2.length === 0 || prevRounds[hash(gs)]) {    
+      let ret = { gs: gs, winner: 1 };
+    //  memo.set(hash(gs), _.cloneDeep(ret))
+      return ret;
     }
-    if (gs.p1.length === 0) return { gs: gs, winner: 2 };
-    prevRounds.push(_.cloneDeep(gs))
+    if (gs.p1.length === 0) {
+      let ret = { gs: gs, winner: 2 };
+    //  memo.set(hash(gs), _.cloneDeep(ret))
+      return ret;
+    }
+    prevRounds[hash(gs)] = true
     let c1 = gs.p1.shift();
     let c2 = gs.p2.shift();
     // recurse
     if (gs.p1.length >= c1 && gs.p2.length >= c2){
       //console.log("recursed")
-      let {winner} = round(_.cloneDeep(gs), [])
+      var hwinner;
+      // if(memo.has(hash(gs))) {
+      //   // if(!_.isEqual(memo.get(hash(gs)).gs, gs)) {
+      //   //   console.log(memo.get(hash(gs)).gs, gs)
+      //   //   throw Error(`Hash collision: `)
+      //   // }
+      //    hwinner = memo.get(hash(gs)).winner
+      // } else {
+        let ret = round(_.cloneDeep(gs), {})
+      //  memo.set(hash(gs), _.cloneDeep(ret))
+        hwinner = ret.winner
+      //}
       //console.log("recursed",winner)
-      if(winner === 1){
+      if(hwinner === 1){
         gs.p1.push(c1,c2)
       } else {
         gs.p2.push(c2,c1)
@@ -142,7 +173,7 @@ let playRec = (gs) => {
       gs.p2.push(c2, c1);
     }
     return round(gs, prevRounds);
-  })(gs, []);
+  })(gs, {});
 };
 
 let testI = `Player 1:
@@ -154,6 +185,9 @@ Player 2:
 29
 14`
 
-//console.log(parse(test))
-//console.log(playRec(parse(input)))
-console.log(score(playRec(parse(input)).gs))
+// console.log(parse(test))
+// console.log(playRec(parse(input)))
+console.log(score(playRec(parse(test)).gs))
+let ret = playRec(parse(input))
+console.log(ret)
+console.log(score(ret.gs))
